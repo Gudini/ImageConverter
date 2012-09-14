63d28afc14307e3975903b2fd9c27e3d2115f640
Sergei Yakimchik <yakimchik7@gmail.com>
2012-09-15 00:32:03 +0400

    first commit. Projet added.

---
+++ ImageConverter/FilterDialog.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ImageConverter
+{
+    /// <summary>
+    /// Interaction logic for FilterDialog.xaml
+    /// </summary>
+    public partial class FilterDialog : Window
+    {
+        public FilterDialog()
+        {
+            InitializeComponent();
+        }
+
+        private String value;
+
+        private void CancelClick(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            pixelTextBox.Text = String.Format("{0:0}", slider.Value);
+        }
+
+        public  String Value
+        {
+            get { return value; }
+        }
+
+        private void OkClick(object sender, RoutedEventArgs e)
+        {
+            value = String.Format("{0:0}", slider.Value);
+            this.Close();
+        }
+    }
+}
---
+++ ImageConverter/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+
+namespace ImageConverter
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OpenFileClick(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.InitialDirectory = "D:\\";
+            dlg.Filter = "Image files (*.jpg)|*.jpg|PNG (*.png)|*.png|BMP (*.bmp)|*.bmp|All Files(*.*)|*.*";
+            dlg.RestoreDirectory = true;
+            if(dlg.ShowDialog()==true)
+            {
+                string selectedFileName = dlg.FileName;
+                BitmapImage bitmapImage = new BitmapImage();
+                bitmapImage.BeginInit();
+                bitmapImage.UriSource = new Uri(selectedFileName);
+                bitmapImage.EndInit();
+                ImageView1.Source = bitmapImage;
+                this.Height = this.Width/2+25;
+            }
+        }
+
+        private void ExitFileClick(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void MedianFilterClick(object sender, RoutedEventArgs e)
+        {
+            var filterDialog = new FilterDialog();
+            filterDialog.ShowDialog();
+            int value = Convert.ToInt32(filterDialog.Value);
+        }
+    }
+}
